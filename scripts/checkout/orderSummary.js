574839df468c26f5f1dc93d2abbd3fb0fbdd9073
ShruthikaBaswa <mail13shruthika@gmail.com>
2025-06-02 18:06:19 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,13 +1,10 @@
 import {cart, removeFromCart,updateDeliveryOption} from '../../data/cart.js';
 import {products,getProduct} from '../../data/products.js';
-import { formatCurrency } from '../utils/money.js';
+import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js';
 
-
-const today=dayjs();
-const deliveryDate=today.add(7,'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary(){
     let cartSummaryHTML='';
@@ -107,6 +104,8 @@ export function renderOrderSummary(){
 
             const container=document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderPaymentSummary();
         });
     });
 
@@ -115,6 +114,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
