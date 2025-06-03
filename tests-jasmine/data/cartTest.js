4360d7715e051e4989ba00ac07d21d3ec62dbde8
ShruthikaBaswa <mail13shruthika@gmail.com>
2025-06-03 15:05:56 +0530

    Create automated tests for orderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -2,11 +2,11 @@ import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart',() => {
     it('adds an existing product to the cart',()=>{
-        spyOn(localStorage,'setItem');
-        spyOn(localStorage,'getItem').and.callFake(() => {
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
                 productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity:1,
+                quantity: 1,
                 deliveryOptionId:'1'
             }]);
         });
@@ -19,9 +19,9 @@ describe('test suite: addToCart',() => {
         expect(cart[0].quantity).toEqual(2);
     });
 
-     it('adds an new product to the cart',()=>{
-        spyOn(localStorage,'setItem');
-        spyOn(localStorage,'getItem').and.callFake(() => {
+    it('adds an new product to the cart',()=>{
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([]);
         });
         loadFromStorage();
